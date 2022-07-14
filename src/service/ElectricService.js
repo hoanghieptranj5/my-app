85611d0a9eb1f540dbba3f5c986125191fa9989c
Hiep Tran <hiep.tran@zoi.de>
2022-07-14 13:34:45 +0700

    change electric URL BE

--- src/service/ElectricService.js
+++ src/service/ElectricService.js
@@ -1,5 +1,5 @@
 
-const URL = "https://d9frjnilz1.execute-api.us-east-2.amazonaws.com/Prod/api/Calculator?usage=";
+const URL = "https://0z8irnko40.execute-api.us-west-1.amazonaws.com/Prod/api/Calculator?usage=";
 
 export const calculate = async (usage) => {
   return await fetch(URL + usage.toString());
